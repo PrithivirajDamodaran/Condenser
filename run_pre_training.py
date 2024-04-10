fef3eb810deb1b32bbb99a79a629b227a7c8cd72
Prithivida <d.prithiviraj@gmail.com>
2024-04-10 19:12:42 +0700

    Update run_pre_training.py

--- run_pre_training.py
+++ run_pre_training.py
@@ -114,6 +114,8 @@ def main():
         data_files=data_args.train_path,
         chunksize=2**25,
     )['train']
+    logger.warning(train_set)
+    
     dev_set = load_dataset(
         'json',
         data_files=data_args.validation_file,
